34eae573aac464ef829d6bbc0094b035f2720974
naelsondouglas <naelson17@gmail.com>
2022-11-20 03:45:18 -0300

    data ok

--- lstm.py
+++ lstm.py
@@ -1,18 +1,19 @@
+import math
+
+import numpy as np
 import pandas as pd
 import seaborn as sns
-# import tensorflow as tf
 from matplotlib import pyplot as plt
+from numpy.lib.stride_tricks import sliding_window_view
 from sklearn.model_selection import train_test_split
-import math
-# from tensorflow import keras
-# from tensorflow.keras import layers
+
 
 def sigmoid(x):
     sig = 1 / (1 + math.exp(-x))
     return sig
 
 def performance(gain:float) -> float:
-    MIN_GAIN = 1.0275
+    MIN_GAIN = 1.0125
     (LOSS, LATERAL, GAIN) = (-1, 0, 1)
     if gain >= MIN_GAIN:
         return GAIN
@@ -21,35 +22,16 @@ def performance(gain:float) -> float:
     else:
         return LATERAL
 
-
-# class Lstm(keras.Sequential):
-#     def __init__(self):
-#         super().__init__()
-#         self.add(layers.LSTM(64, input_shape=(None, 28)))
-#         self.add(layers.BatchNormalization())
-#         self.add(layers.Dense(10))
-#         self.compile(
-#             loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-#             optimizer='sgd',
-#             metrics=['accuracy'],
-#         )
-
 def calc_metrics(df:pd.DataFrame) -> pd.DataFrame:
-    df = df[['date', 'open', 'close', 'volume_24h']]
-    df['open-1'] = df['open'].shift(1)
-    df['volume-1'] = df['volume_24h'].shift(1)
-    df['openopen'] = df['open']/df['open-1']
-    df['volumevolume'] = df['volume_24h']/df['volume-1']
-    df['gain'] = df['close']/df['open']
-    df['result'] = df['gain'].apply(performance)
+    df = df[['open_time', 'open', 'close', 'volume']]
+    df['volumevolume'] = df['volume'].shift(1)/df['volume'].shift(2)
+    df['previous_gain'] = df['close']/df['open']
+    df['previous_gain'] = df['previous_gain'].shift(1)
+    df['result'] = df['previous_gain'].apply(performance)
     return df
 
-def get_df(source_file:str, other_id:int) -> pd.DataFrame:
-    df = pd.read_csv(source_file)
-    other = calc_metrics(df.loc[df['coin_id'] == other_id])
-    btc = calc_metrics(df.loc[df['coin_id'] == 1])
-    btc = btc.loc[btc.date.isin(other.date)]
-    other['btc_gain'] = btc['gain'].apply(lambda x: round(x, 5))
+def add_btc_gain(other:pd.DataFrame, btc:pd.DataFrame) -> pd.DataFrame:
+    other['previous_btc_gain'] = btc['previous_gain'].apply(lambda x: round(x, 5))
     return other
 
 def train_test_val(df:pd.DataFrame) -> tuple:
@@ -57,13 +39,37 @@ def train_test_val(df:pd.DataFrame) -> tuple:
     test, val = train_test_split(test_val, train_size=0.5, shuffle=False)
     return (train, test, val)
 
+def make_sample(window:np.array) -> np.array:
+    window = window[0][0]
+    nof_features = window.shape[-1]
+    features = window[:,0:nof_features-1].T
+    target = window[:,-1]
+
+def get_windows(df:pd.DataFrame, window_size:int):
+    min_class = df['result'].value_counts().min()
+    classes = df['result'].value_counts().reset_index()
+    classes = {class_value:nof_samples for (class_value,nof_samples) in  classes.values}
+    nof_features = len(df.columns)
+    windows = sliding_window_view(df.values, (window_size, nof_features))
+    windows = windows.reshape(len(windows), window_size, nof_features)
+    result = list()
+    for (class_value, nof_samples) in classes.items():
+        samples = np.array([w for w in windows if w[-1,-1] == class_value])
+        idx = np.random.randint(len(samples), size=min_class)
+        samples = samples[idx,:]
+        result.append(samples)
+    return np.vstack(result)
+
 if __name__ == '__main__':
-    df = get_df('nano_btc.csv',2)
-    # df = df.loc[df['result'].isin([-1, 1])]
-    # df = df.sample(1500)
-    df = df[['openopen', 'volumevolume', 'result']]
-    df['openopen'] = df['openopen'].apply(lambda x: math.lgamma(x))
-    df['volumevolume'] = df['volumevolume'].apply(lambda x: math.lgamma(math.sqrt(x)))
-    sns.scatterplot(df, x='volumevolume', y='openopen', hue='result')
-    plt.show()
-    # model = Lstm()
+    ltc = pd.read_parquet('ltc.parquet')
+    ltc = calc_metrics(ltc)
+
+    btc = pd.read_parquet('btc.parquet')
+    btc = calc_metrics(btc)
+
+    ltc = add_btc_gain(ltc, btc)
+
+    ltc = ltc[['open_time', 'open', 'volume', 'volumevolume', 'previous_gain', 'previous_btc_gain', 'result']]
+    ltc = ltc[['volumevolume', 'previous_gain', 'previous_btc_gain', 'result']]
+    ltc = ltc.dropna()
+    windows = get_windows(ltc, 24)
